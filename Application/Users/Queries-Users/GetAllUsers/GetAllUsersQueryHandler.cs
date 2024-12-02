b1764155ec78ce2b5aa73358b0543b0fe47d400e
KungSimon <simon.naslund@hotmail.com>
2024-12-02 13:41:37 +0100

    Fixed JWT-token

---
+++ Application/Users/Queries-Users/GetAllUsers/GetAllUsersQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Infrastructure.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.Queries_Users.GetAllUsers
+{
+    public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, List<User>>
+    {
+        private readonly FakeDatabase _fakeDatabase;
+
+        public GetAllUsersQueryHandler(FakeDatabase fakeDatabase)
+        {
+            _fakeDatabase = fakeDatabase;
+        }
+        public Task<List<User>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            List<User> allUsersFromFakeDB = _fakeDatabase.Users;
+            return Task.FromResult(allUsersFromFakeDB);
+        }
+    }
+}
---
+++ Application/Users/Queries-Users/LoginUsers/LoginUserQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Users.Queries_Users.LoginUsers.Helpers;
+using Infrastructure.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.Queries_Users.LoginUsers
+{
+    public class LoginUserQueryHandler : IRequestHandler<LoginUserQuery, string>
+    {
+        private readonly FakeDatabase _fakeDatabase;
+        private readonly TokenHelper _tokenHelper;
+
+        public LoginUserQueryHandler(FakeDatabase fakeDatabase, TokenHelper tokenHelper)
+        {
+            _fakeDatabase = fakeDatabase;
+            _tokenHelper = tokenHelper;
+        }
+        public Task<string> Handle(LoginUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = _fakeDatabase.Users.FirstOrDefault(user => user.UserName == request.LoginUser.UserName && user.Password == request.LoginUser.Password);
+
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("Invalid username or password");
+            }
+
+            string token = _tokenHelper.GenerateJwtToken(user);
+
+            return Task.FromResult(token);
+        }
+    }
+}
